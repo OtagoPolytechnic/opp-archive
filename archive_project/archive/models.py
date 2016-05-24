c3a15ac8b2f1735957cbe6996a2bbfc6b03f8c17
doglitbug <s_drac2@yahoo.com>
2016-05-24 21:15:41 +1200

    Created models and added to admin page

--- archive_project/archive/models.py
+++ archive_project/archive/models.py
@@ -3,3 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    organization = models.CharField(max_length = 128, default = 'None')
+    person = models.CharField(max_length = 128, blank = True)
+    contact = models.CharField(max_length = 128, blank = True)
+
+    def __unicode__(self):
+        return self.organization + ', ' + self.person
+
+class Project(models.Model):
+    year = models.IntegerField()
+    groupName = models.CharField(max_length = 128, unique = True)
+    client = models.ForeignKey(Client)
+
+    def __unicode__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length = 128, unique = True, null = False)
+    project = models.ForeignKey(Project)
+
+    def __unicode__(self):
+        return self.name
