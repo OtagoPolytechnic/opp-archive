8b89379076a9603150517eb595ba665214092b2b
doglitbug <s_drac2@yahoo.com>
2016-05-24 21:15:41 +1200

    Created models and added to admin page

--- archive_project/archive/admin.py
+++ archive_project/archive/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from archive.models import Client, Project, Student
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(Student)
