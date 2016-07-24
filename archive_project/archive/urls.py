d818601aeac9c4d1cce03d1a194b725f367b5984
doglitbug <s_drac2@yahoo.com>
2016-07-24 12:03:49 +1200

    Refactored into 4 pages and added in a whole lot of data validation

--- archive_project/archive/urls.py
+++ archive_project/archive/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from archive import views
+from forms import SearchForm
 
 urlpatterns = [
      url(r'^$', views.index, name='index'),
-     url(r'search/$', views.search, name='search'),
-     url(r'request/$', views.request, name='request'),
+     url(r'results/$', views.results, name='results'),
+     url(r'details/$', views.details, name='details'),
+     url(r'confirmation/$', views.confirmation, name='confirmation'),
 ]
